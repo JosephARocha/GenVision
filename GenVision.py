bf9215046935be0e020f17cb5616fdb4ec9fea82
JosephRocha <RochaTrompeta>
2017-12-03 23:54:30 -0600

    Final Update Before Turnin

--- GenVision.py
+++ GenVision.py
@@ -1,8 +1,7 @@
-#from __future__ import print_function
 from keras.layers import Conv2D,MaxPooling2D
 from keras.layers import Activation,Dropout,Flatten,Dense
 from keras.models import Sequential
-from PIL import Image
+from PIL import Image, ImageOps
 from keras.preprocessing.image import img_to_array
 from keras import optimizers
 import numpy as np
@@ -10,48 +9,49 @@ import os
 from numpy import array
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 import sys
-model = Sequential()
-#dire = ''
-#folder = os.listdir(dire)
 
+model = Sequential()
 
 img_width,img_height = 150,150
 input_shape = (img_width, img_height, 3)
 
-model.add(Conv2D(32, (3, 3), input_shape=input_shape))
-model.add(Activation('relu'))
-model.add(Conv2D(32,(3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+model = Sequential()
 
-model.add(Conv2D(64, (3, 3)))
+model.add(Conv2D(64,(3,3), input_shape=input_shape))
 model.add(Activation('relu'))
-model.add(Conv2D(64, (3, 3)))
+model.add(Conv2D(64, (3,3)))
 model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(MaxPooling2D(pool_size=(3, 3)))
 
-model.add(Conv2D(128, (3, 3)))
+model.add(Conv2D(32, (3, 3)))
 model.add(Activation('relu'))
-model.add(Conv2D(128, (3, 3)))
+model.add(Conv2D(32, (3, 3)))
 model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(MaxPooling2D(pool_size=(3,3)))
+
+model.add(Conv2D(16, (3, 3)))
+model.add(Activation('relu'))
+model.add(Conv2D(16, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(3,3))) 
 
 model.add(Flatten()) 
-model.add(Dense(128))
+model.add(Dense(16))
 model.add(Activation('relu'))
 
 model.add(Dense(1))
 model.add(Activation('sigmoid'))
-model.load_weights('gender_try.h5')
-optimizer= optimizers.RMSprop(lr=0.0001, rho=0.9, epsilon=1e-08, decay=0.0)
-model.compile(loss='binary_crossentropy',optimizer='rmsprop',metrics =['accuracy'])
+
+model.load_weights('GenVision.h5')
+myoptimizer= optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.000066)
+model.compile(loss='binary_crossentropy',optimizer=myoptimizer,metrics =['accuracy'])
 
 things_to_classify = []
 
 data = sys.argv[1]
 print(data)
 img = Image.open(data)
-img = img.resize((150, 150))
+img = ImageOps.fit(img, (150, 150), Image.ANTIALIAS, 0, (0.5, 0.5))
 things_to_classify.append(np.asarray(img, dtype=np.uint8))
 
 arr = array(things_to_classify)
